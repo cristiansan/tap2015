accdefdfb3791e714de25f64b0673182fa6e605d
home <matensegiles@gmail.com>
2015-07-05 20:12:21 -0300

    Initial commit

---
+++ app/src/main/java/app/tap/Fotos.java
@@ -0,0 +1,22 @@
+package app.tap;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ITX on 7/5/2015.
+ */
+public class Fotos extends Fragment {
+
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_fotos, container, false);
+            return rootView;
+        }
+
+}
---
+++ app/src/main/java/app/tap/Ranking.java
@@ -0,0 +1,23 @@
+package app.tap;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ITX on 7/5/2015.
+ */
+public class Ranking extends Fragment {
+
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_ranking, container, false);
+            return rootView;
+        }
+
+}
